3c76f861645a97e174a9da6c2033b0beb23d37b9
Jon Fuller <jcfuller@sep.com>
2022-03-22 12:22:08 -0400

    enable/disable based on tab, also set badge

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Installed Coda-Scroll-To-Here");
 });
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
+  });
+});
+
+chrome.tabs.onActivated.addListener(({tabId, windowId}, _) => {
+  console.log(tabId);
+  chrome.tabs.get(tabId, async (tab) => {
+      const shouldEnable = tab.url.startsWith("https://coda.io/d/");
+
+      if (shouldEnable) {
+        chrome.action.enable({tabId});
+        chrome.action.setBadgeText({
+          text: "✅",
+          tabId: tabId});
+        }
+      else {
+        chrome.action.disable({tabId});
+        chrome.action.setBadgeText({
+          text: "⛔",
+          tabId: tabId});
+        }
+    });
+});
---
+++ content.js
@@ -0,0 +1,7 @@
+var title = document.querySelector("div[data-coda-ui-id='page-title']").firstChild.textContent;
+var pageItems = document.querySelectorAll("div[data-coda-ui-id='page-list-item']");
+var pageItem = Array.from(pageItems)
+  .map(item => item.querySelector("textarea"))
+  .find(item => item.textContent === title);
+
+pageItem.scrollIntoView();
