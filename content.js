f93f68492001fc3a25fc671c83c96fd25aca4eba
Jon Fuller <jcfuller@sep.com>
2022-03-22 13:50:12 -0400

    smooth scrolling

--- content.js
+++ content.js
@@ -4,4 +4,4 @@ var pageItem = Array.from(pageItems)
   .map(item => item.querySelector("textarea"))
   .find(item => item.textContent === title);
 
-pageItem.scrollIntoView();
+pageItem.scrollIntoView({behavior: 'smooth'});
