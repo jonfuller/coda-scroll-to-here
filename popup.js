751c76efd64552a0accedbbaf35196ea3c5ab8fd
Jon Fuller <jcfuller@sep.com>
2022-03-22 11:42:23 -0400

    trimmed out the fat - no longer any 'change color' stuff lingering

--- popup.js
+++ popup.js
@@ -1,26 +1,22 @@
-let changeColor = document.getElementById("changeColor");
+let scrollButton = document.getElementById("scrollToCurrent");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-changeColor.addEventListener("click", async () => {
+scrollButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: scrollToCurrent,
   });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
+function scrollToCurrent() {
   var title = document.querySelector("div[data-coda-ui-id='page-title']").firstChild.textContent;
   var pageItems = document.querySelectorAll("div[data-coda-ui-id='page-list-item']");
-  var whatever = Array.from(pageItems)
+  var pageItem = Array.from(pageItems)
     .map(item => item.querySelector("textarea"))
     .find(item => item.textContent === title);
 
-  whatever.scrollIntoView();
+  pageItem.scrollIntoView();
 }
